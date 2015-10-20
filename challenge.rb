95e34297b2a539d8944270f2ab11caecf810b7e5
Teo Paolucci <teo.paolucci@gmail.com>
2015-10-20 09:24:22 -0400

    Ginished up challenge.rb
    Used Google, Ruby documentation, and notes from the lesson to complete.

--- challenge.rb
+++ challenge.rb
@@ -8,6 +8,10 @@
 33
 65.89
 
+#ANSWER:
+
+.class
+
 # Question 2: Booleans
 # What would be the result of executing the below statements
 
@@ -16,20 +20,28 @@ if 33 && 55
 else
   puts "it's false"
 end
+# ANSWER:
+
+"it's true"
 
 # 2.1 Substitue "hello" && 4.5 for 33 && 55 above. What's the output?
+"it's true"
 # 2.2 Substitue nil && 88 for 33 && 55 above. What's the output?
+"it's false"
 # 2.4 Substitue 63.4 < 64. What's the output?
+"it's true"
 
 # Question 3 :Strings
 
 # How would you (Yes, you can look up the String methods!):
 
-# 3.1. determine if a string is included another another string?
+# 3.1. determine if a string is included in another string?
+"another string".include? "string"
 # 3.2. determine if the length of a string?
+"string".length
 # 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't
 # change the string "eels in my hovercraft"
-
+new_string = "eels in my hovercraft".gsub(/eels/, "fish").gsub(/hovercraft/, "sink")
 
 # Question 4 : Variables
 # Change the below string, do NOT make copies of the string. All changes
@@ -38,9 +50,12 @@ end
 vehicle = "4 wheel Bus"
 
 # 4.1 to "6 wheel Bus"
+vehicle.gsub!(/4/, "6")
 # 4.2 to "6 wheel tank"
+vehicle.gsub!(/Bus/, "tank")
 # 4.3 Create a NEW string from the string in 4.2 that will be
 # "6 wheel tank that shoots rainbow bunnnies"
+best_vehicle = vehicle << " that shoots rainbow bunnies"
 
 # Question 5 : Loop
 # - write a loop that guesses your age.
@@ -51,8 +66,29 @@ vehicle = "4 wheel Bus"
 # - I'm not kidding, say unkind works to a user that thinks your 10 years older
 #   than you are.
 
+age = 20
+guess = nil
+num_guesses = 0
+
+puts "Guess my age..."
+while (guess != age) && (num_guesses <= 4)
+  num_guesses += 1
+  guess = gets.chomp!.to_i
+  if guess > (10 + age)
+    puts "Wow. You suck. You don't get to try anymore. >:C"
+    break
+  elsif guess != age
+    puts "Nope. Try again."
+  end
+end
+
+puts "Good stuff! You guessed right!" if guess == age
+
 # Bonus Question: Who said this?
 
 # The great enemy of the truth is very often not the lie -- deliberate, contrived
 # and dishonest, but the myth, persistent, persuasive, and unrealistic.
-# Belief in myths allows the comfort of opinion without the discomfort of thought.
+# Belief in myths allows the comfort of opinion without the discomfort of thought
+
+# John F. Kennedy
+# Thanks Google!
